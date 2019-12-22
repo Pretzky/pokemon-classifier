4f6b540ce0c960e8f951a239f25d63b1b4830134
Pretzky <prestonj.russell@gmail.com>
2019-12-21 22:39:09 -0700

    Updates

---
+++ precache-manifest.885df5066e14ba54cba062d1d62f733f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fb18b917dc0a51ba73ea53b46cdaae",
+    "url": "/pokemon-classifier/index.html"
+  },
+  {
+    "revision": "f0b199107c51a91a0bc7",
+    "url": "/pokemon-classifier/static/css/main.041ddd81.chunk.css"
+  },
+  {
+    "revision": "7f5fafd5ae7016fdd29c",
+    "url": "/pokemon-classifier/static/js/2.275635b4.chunk.js"
+  },
+  {
+    "revision": "d3f0a422d8ec643000b73dd5acfeb768",
+    "url": "/pokemon-classifier/static/js/2.275635b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0b199107c51a91a0bc7",
+    "url": "/pokemon-classifier/static/js/main.7d1334d7.chunk.js"
+  },
+  {
+    "revision": "a9c4ec6f9ef8162ceb3b",
+    "url": "/pokemon-classifier/static/js/runtime-main.39946285.js"
+  }
+]);
